3fcaef2b7d7dadc9e7a88201bb0c3575f461e8f4
Lokendra Chaulagain <lokendrachaulagain803@gmail.com>
2024-11-03 21:10:16 +0545

    fix

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 export async function createClient() {
   const cookieStore = await cookies();
 
-  return createServerClient(process.env.NEXT_SUPABASE_URL!, process.env.NEXT_SERVICE_KEY!, {
+  return createServerClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SECRET_KEY!, {
     cookies: {
       getAll() {
         return cookieStore.getAll();
