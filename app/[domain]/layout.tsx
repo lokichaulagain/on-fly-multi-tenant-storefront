7ea1b3db87e84ed26cd26bf5c6907211b938b112
Lokendra Chaulagain <lokendrachaulagain803@gmail.com>
2025-01-30 19:53:27 +0545

    ,,

---
+++ app/[domain]/layout.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+// import CTA from "@/components/cta";
+// import ReportAbuse from "@/components/report-abuse";
+import { notFound, redirect } from "next/navigation";
+// import { getSiteData } from "@/lib/fetchers";
+// import { fontMapper } from "@/styles/fonts";
+import { Metadata } from "next";
+import { getStoreBySubdomain } from "@/actions/store";
+
+// export default async function SiteLayout({ params, children }: { params: { domain: string }; children: ReactNode }) {
+export default async function SiteLayout({ children, params }: { children: ReactNode; params: Promise<{ domain: string }> }) {
+  const { domain } = await params;
+  const decodedDomain = decodeURIComponent(domain);
+  console.log(decodedDomain, "This is domain");
+    const response = await getStoreBySubdomain(domain);
+    console.log(response, "This is response");
+
+    if (!response) {
+      notFound();
+    }
+
+  //   // Optional: Redirect to custom domain if it exists
+  //   if (domain.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) && data.customDomain && process.env.REDIRECT_TO_CUSTOM_DOMAIN_IF_EXISTS === "true") {
+  //     return redirect(`https://${data.customDomain}`);
+  //   }
+
+  return (
+    // <div className={fontMapper[data.font]}>
+    //   <div className="ease left-0 right-0 top-0 z-30 flex h-16 bg-white transition-all duration-150 dark:bg-black dark:text-white">
+    //     <div className="mx-auto flex h-full max-w-screen-xl items-center justify-center space-x-5 px-10 sm:px-20">
+    //       <Link
+    //         href="/"
+    //         className="flex items-center justify-center">
+    //         <div className="inline-block h-8 w-8 overflow-hidden rounded-full align-middle">
+    //           <Image
+    //             alt={data.name || ""}
+    //             height={40}
+    //             src={data.logo || ""}
+    //             width={40}
+    //           />
+    //         </div>
+    //         <span className="ml-3 inline-block truncate font-title font-medium">{data.name}</span>
+    //       </Link>
+    //     </div>
+    //   </div>
+
+    //   <div className="mt-20">{children}</div>
+
+    //   {domain == `demo.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}` || domain == `platformize.co` ? <CTA /> : <ReportAbuse />}
+    // </div>
+
+    <div>
+      <div className="mt-20">{children}</div>
+    </div>
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -1,76 +1,3 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { NextResponse } from "next/server";
-// import { readSiteDomain } from "./utils/actions/sites/read-site-domain";
-
-// // Define the routes that require authentication
-// const isProtectedRoute = createRouteMatcher(["/cms(.*)"]);
-
-// // Main middleware function
-// export default clerkMiddleware(async (auth, req) => {
-//   // Check if the route is protected and enforce authentication if it is
-//   if (isProtectedRoute(req)) auth().protect();
-
-//   const url = req.nextUrl;
-//   const pathname = url.pathname;
-
-//   // Get hostname (e.g., 'mike.com', 'test.mike.com')
-//   const hostname = req.headers.get("host");
-
-//   console.log("Hostname:", hostname);
-
-//   let currentHost;
-//   if (process.env.NODE_ENV === "production") {
-//     // Production logic remains the same
-//     const baseDomain = process.env.BASE_DOMAIN;
-//     currentHost = hostname?.replace(`.${baseDomain}`, "");
-//   } else {
-//     // Updated development logic
-//     currentHost = hostname?.split(":")[0].replace(".localhost", "");
-//   }
-//   // If there's no currentHost, likely accessing the root domain, handle accordingly
-//   if (!currentHost) {
-//     // Continue to the next middleware or serve the root content
-//     return NextResponse.next();
-//   }
-
-//   // Fetch tenant-specific data based on the hostname
-//   const response = await readSiteDomain(currentHost);
-
-//   if (!Array.isArray(response) || response.length === 0) {
-//     return NextResponse.next();
-//   }
-
-//   // Handle the case where no domain data is found
-//   if (!response || !response.length) {
-//     // Continue to the next middleware or serve the root content
-//     return NextResponse.next();
-//   }
-
-//   const site_id = response[0]?.id;
-//   const tenantSubdomain = response[0]?.subdomain;
-//   //   const mainDomain = response[0]?.site_custom_domain;
-
-//   // Determine which domain to use for rewriting
-//   const rewriteDomain = tenantSubdomain; // || mainDomain;
-
-//   console.log("Hostname:", hostname);
-//   console.log("Current Host:", currentHost);
-//   console.log("Rewrite Domain:", rewriteDomain);
-
-//   if (rewriteDomain) {
-//     // Rewrite the URL to the tenant-specific path, using the site_id
-//     return NextResponse.rewrite(new URL(`/${site_id}${pathname}`, req.url));
-//   }
-
-//   // If no rewrite domain is found, continue to the next middleware
-//   return NextResponse.next();
-// });
-
-// // Define which paths the middleware should run for
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
@@ -91,16 +18,25 @@ export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  let hostname = req.headers.get("host")!.replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+  let hostname = req.headers
+    .get("host")!
+    .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
   // special case for Vercel preview deployment URLs
-  if (hostname.includes("---") && hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)) {
-    hostname = `${hostname.split("---")[0]}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
+  if (
+    hostname.includes("---") &&
+    hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)
+  ) {
+    hostname = `${hostname.split("---")[0]}.${
+      process.env.NEXT_PUBLIC_ROOT_DOMAIN
+    }`;
   }
 
   const searchParams = req.nextUrl.searchParams.toString();
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
-  const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
+  const path = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ""
+  }`;
 
   // rewrites for app pages
   if (hostname == `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
@@ -110,17 +46,26 @@ export default async function middleware(req: NextRequest) {
     } else if (session && path == "/login") {
       return NextResponse.redirect(new URL("/", req.url));
     }
-    return NextResponse.rewrite(new URL(`/app${path === "/" ? "" : path}`, req.url));
+    return NextResponse.rewrite(
+      new URL(`/app${path === "/" ? "" : path}`, req.url),
+    );
   }
 
   // special case for `vercel.pub` domain
   if (hostname === "vercel.pub") {
-    return NextResponse.redirect("https://vercel.com/blog/platforms-starter-kit");
+    return NextResponse.redirect(
+      "https://vercel.com/blog/platforms-starter-kit",
+    );
   }
 
   // rewrite root application to `/home` folder
-  if (hostname === "localhost:3000" || hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
-    return NextResponse.rewrite(new URL(`/home${path === "/" ? "" : path}`, req.url));
+  if (
+    hostname === "localhost:3000" ||
+    hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
+  ) {
+    return NextResponse.rewrite(
+      new URL(`/home${path === "/" ? "" : path}`, req.url),
+    );
   }
 
   // rewrite everything else to `/[domain]/[slug] dynamic route
