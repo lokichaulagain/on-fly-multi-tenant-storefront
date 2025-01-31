f02e73a001301fcc4abe49955fcf8aa559bf1511
Lokendra Chaulagain <lokendrachaulagain803@gmail.com>
2025-01-31 10:13:03 +0545

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@
 // import { Providers } from "./providers";
 import { Metadata } from "next";
 // import { cn } from "@/lib/utils";
+import "./globals.css";
 
 const title = "Platforms Starter Kit – The all-in-one starter kit for building multi-tenant applications.";
 const description = "The Platforms Starter Kit is a full-stack Next.js app with multi-tenancy and custom domain support. Built with Next.js App Router, Vercel Postgres and the Vercel Domains API.";
