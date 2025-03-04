131e6ca24d991b897c36e4f2b7655a8b57f6852a
Lokendra Chaulagain <lokendrachaulagain803@gmail.com>
2025-03-04 10:00:41 +0545

    fix

--- actions/store.ts
+++ actions/store.ts
@@ -33,8 +33,14 @@ export async function getStoreBySubdomain(subdomain: string): Promise<ActionResp
   1. Get store metadata
 */
 
-export async function getActiveStoreMetadata(store_subdomain: string): Promise<ActionResponse<StoreMetadata>> {
+export async function getActiveStoreMetadata(): Promise<ActionResponse<StoreMetadata>> {
   try {
+    // 1. Get subdomain from headers
+    const headersList = await headers();
+    const host = headersList.get("host") || "";
+    const store_subdomain = host.split(".")[0];
+    console.log(store_subdomain, "This is store_subdomain from getActiveStoreMetadata");
+
     // 2. Get store metadata from store table
     const [store] = await db
       .select({
@@ -52,7 +58,7 @@ export async function getActiveStoreMetadata(store_subdomain: string): Promise<A
       .limit(1);
 
     // 3. Check if store exists
-    if (store) {
+    if (!store) {
       return { data: null, error: "Store not found", status: 404 };
     }
 
