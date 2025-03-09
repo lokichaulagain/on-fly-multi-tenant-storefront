4447cffc34f85f84ce686ccc4a4d40cd5906e170
Lokendra Chaulagain <lokendrachaulagain803@gmail.com>
2025-03-09 09:54:10 +0545

    error handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,36 +3,39 @@ import "./globals.css";
 import { getActiveStore } from "@/actions/store";
 import { CurrentStoreProvider } from "@/contexts/current-store-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { NoStoreFound } from "@/components/no-store-found";
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const response = await getActiveStore();
-  const store_appearance = response?.data?.store_appearance;
 
-  if (response.error || !response.data || !store_appearance) {
-    return <div>Error fetching store appearance</div>;
+  if (response.error || !response.data) {
+    return (
+      <html lang="en">
+        <body>
+          <NoStoreFound />
+        </body>
+      </html>
+    );
   }
 
   return (
     <ClerkProvider dynamic>
       <CurrentStoreProvider store={response.data}>
-        <html
-          lang="en"
-          suppressHydrationWarning>
+        <html lang="en">
           <head>
             <style>
               {`
               :root {
-                --font-family: ${store_appearance.font_family};
-                --primary: ${store_appearance.primary_color};
-                --secondary: ${store_appearance.secondary_color};
-                --radius: ${store_appearance.border_radius}px;
-                --ring: ${store_appearance.primary_color} 
+                --font-family: ${response.data.store_appearance?.font_family};
+                --primary: ${response.data.store_appearance?.primary_color};
+                --secondary: ${response.data.store_appearance?.secondary_color};
+                --radius: ${response.data.store_appearance?.border_radius}px;
+                --ring: ${response.data.store_appearance?.primary_color} 
               }
             `}
             </style>
           </head>
           <body>
-            
             {children}
             <Toaster />
           </body>
