7a2098e4d701882b3c311f97a011cba0a65521b3
Loki Chaulagain <lokendrachaulagain803@gmail.com>
2025-03-10 09:45:11 +0545

    Update page.tsx

--- app/[domain]/p/[slug]/page.tsx
+++ app/[domain]/p/[slug]/page.tsx
@@ -14,7 +14,7 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
 
   return (
     <div className="container mx-auto px-4 md:px-24 min-h-screen">
-      <div>{parse(response.data.content || "")}</div>
+       <div className="prose prose-lg prose-p:text-lg prose-p:text-muted-foreground max-w-none">{parse(response.data.content || "")}</div>
     </div>
   );
 }
